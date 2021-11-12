466b54b8b744ca674d3d7184b34a37746586dd91
a3032929 <91320050+a3032929@users.noreply.github.com>
2021-11-12 17:31:08 +0000

    graph: updated bar chart

--- graphs/run3totalview.R
+++ graphs/run3totalview.R
@@ -1,7 +1,5 @@
 # plot horizontal bar chart of video topic vs total number of viewers
-topic_view <- run3unite %>% select(1, 5)
-
-bar_topicV <- ggplot(data = topic_view, aes(x=step_title, y=total_views))+
+bar_topicV <- ggplot(data = run3unite, aes(x=step_title, y=total_views))+
   geom_bar(stat = 'identity', fill="#0099f9")+
   geom_text(aes(label=total_views), hjust=-0.2, size=3.5)+
   labs(title="Figure XX: Total number of Viewed per Video Topic")+
