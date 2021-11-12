af0ff494be3d8624d0a786127e0e89a667360c3b
a3032929 <91320050+a3032929@users.noreply.github.com>
2021-11-12 21:23:48 +0000

    graph: updated graphs

--- graphs/scatMviewPct.R
+++ graphs/scatMviewPct.R
@@ -8,15 +8,15 @@
 pairs(~video_duration + viewed_five_percent + viewed_ten_percent + viewed_twentyfive_percent + viewed_fifty_percent + 
         viewed_seventyfive_percent+ viewed_ninetyfive_percent + viewed_onehundred_percent, 
       data = run3unite,
-      main = "No. of views through the Video duration", pch = 19, lower.panel = panel.smooth)
+      main = "No. of views through the Video duration", pch = 19, upper.panel = NULL)
 
 # shows density functions of each variables/ variable distribution in the diagonal
 # top half shows correlations coeeficient (Pearson) for each of the variables
-library(GGally)
+#library(GGally)
 
-run3unite %>% ggpairs(columns = c("video_duration", "viewed_five_percent", "viewed_ten_percent", "viewed_twentyfive_percent",
+#run3unite %>% ggpairs(columns = c("video_duration", "viewed_five_percent", "viewed_ten_percent", "viewed_twentyfive_percent",
                                   "viewed_fifty_percent", "viewed_seventyfive_percent", "viewed_ninetyfive_percent",
                                   "viewed_onehundred_percent"),
-                      upper = list(continuous = wrap('cor', size =4)),
-                      lower = list(continuous = wrap("smooth")),
-                      title = "No. of views through the Video duration")   
+                      #upper = list(continuous = wrap('cor', size =4)),
+                      #lower = list(continuous = wrap("smooth")),
+                      #title = "No. of views through the Video duration")   
--- graphs/scatMviewglobal.R
+++ graphs/scatMviewglobal.R
@@ -8,15 +8,15 @@
 pairs(~video_duration + europe_views_percentage + oceania_views_percentage + asia_views_percentage + north_america_views_percentage + 
       south_america_views_percentage + africa_views_percentage+ antarctica_views_percentage, 
       data = run3unite,
-      main = "Continent views through the Video duration", pch = 19, lower.panel = panel.smooth)
+      main = "Continent views through the Video duration", pch = 19, upper.panel = NULL)
 
 # shows density functions of each variables/ variable distribution in the diagonal
 # top half shows correlations coeeficient (Pearson) for each of the variables
-library(GGally)
+#library(GGally)
 
-run3unite %>% ggpairs(columns = c("video_duration", "europe_views_percentage", "oceania_views_percentage", "asia_views_percentage",
+#run3unite %>% ggpairs(columns = c("video_duration", "europe_views_percentage", "oceania_views_percentage", "asia_views_percentage",
                                   "north_america_views_percentage", "south_america_views_percentage", "africa_views_percentage",
                                   "antarctica_views_percentage"),
-                      upper = list(continuous = wrap('cor', size =4)),
-                      lower = list(continuous = wrap("smooth")),
-                      title = "Continent views through the Video duration")   
+                    #  upper = list(continuous = wrap('cor', size =4)),
+                     # lower = list(continuous = wrap("smooth")),
+                     # title = "Continent views through the Video duration")   
