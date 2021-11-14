f83b18df0a8783f407713e58171f696242a392f6
a3032929 <91320050+a3032929@users.noreply.github.com>
2021-11-14 15:27:47 +0000

    feat: added new feat in munge

--- graphs/pctcontbar.R
+++ graphs/pctcontbar.R
@@ -1,12 +1,7 @@
-dfcont <- continent_pct_ro %>% 
-  select(-video_duration) %>% 
-  gather(key = Continent, value = Value, europe_views_percentage:antarctica_views_percentage)
-dfcont
-
 ggplot(dfcont, aes(x=step_title, y=Value, fill=Continent)) +
   geom_bar(stat = "identity")+
   theme_classic()+
   coord_flip()+
   scale_x_discrete(name = "step_title", labels=function(x) str_wrap(x, width = 32), limits=rev)
 
-# this is to test git resets
+
