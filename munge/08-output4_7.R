e59c67c417cbb6dcc68014c687245f41a025c63c
a3032929 <91320050+a3032929@users.noreply.github.com>
2021-11-16 17:23:57 +0000

    feat: added functions for runs

---
+++ munge/08-output4_7.R
@@ -0,0 +1,12 @@
+# call col_summary function
+output_4 <- col_4_summary(colstouse)
+cache('output_4')
+
+output_5 <- col_5_summary(colstouse)
+cache('output_5')
+
+output_6 <- col_6_summary(colstouse)
+cache('output_6')
+
+output_7 <- col_7_summary(colstouse)
+cache('output_7')
