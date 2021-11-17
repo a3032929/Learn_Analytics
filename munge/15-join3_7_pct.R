ddd1d9ffd7e28fb1b00e7f58df29da6dcfb8be97
a3032929 <91320050+a3032929@users.noreply.github.com>
2021-11-17 17:51:08 +0000

    feat: new feat to join data

---
+++ munge/15-join3_7_pct.R
@@ -0,0 +1,18 @@
+# make pct for each continent
+joint_data_pct <- joint_data / joint_data %>% lapply(max) * 100
+
+# copy the step_title and video duration columns to new pct df
+joint_data_pct$step_title <- joint_data$step_title
+joint_data_pct$video_duration <- joint_data$video_duration
+
+cache('joint_data_pct')
+
+# run3 reorganise columns
+# remove decimal points
+joint_data_pct_ro <- joint_data_pct[,c(8,9,1,2,3,4,5,6,7)] %>% 
+  mutate_if(is.numeric, round) 
+
+# replace NaN with 0
+joint_data_pct_ro[is.na(joint_data_pct_ro)] <- 0
+
+cache('joint_data_pct_ro')
