8264a4087cfd5d45f57d659652528600b8ad5aec
a3032929 <91320050+a3032929@users.noreply.github.com>
2021-11-18 14:13:20 +0000

    feat: new feat to transform df

---
+++ munge/16-new_joint_data_abv_df.R
@@ -0,0 +1,4 @@
+# transform all_runs_absolute_value_df to enable graph plot
+new_cont_all_df_AbV <- joint_data %>% 
+  select(-video_duration) %>% 
+  gather(key = Continent, value = Value, europe_views:antarctica_views)
