2ffdfe623f44d6fa686c6889e351c619c454a0ea
a3032929 <91320050+a3032929@users.noreply.github.com>
2021-11-18 13:16:52 +0000

    doc: tided and updated rmd

--- graphs/stack_cont3_pct_topic_bar.R
+++ graphs/stack_cont3_pct_topic_bar.R
@@ -3,9 +3,9 @@ ggplot(new_cont_3_df, aes(x=step_title, y=Value, fill=Continent)) +
   geom_bar(stat = "identity")+
   theme_classic()+
   coord_flip()+
-  scale_x_discrete(name = "Percentage viewers per continent", labels=function(x) str_wrap(x, width = 32), limits=rev)+
+  scale_x_discrete(name = "Video Topics", labels=function(x) str_wrap(x, width = 32), limits=rev)+
   ggtitle("Figure XX: Percentage of continental viewers on video topics")+
   theme(plot.title = element_text(size=12, face="bold"))+
-  scale_y_discrete(name = "Video Topics")
-
+  scale_color_hue(labels=c("Africa", "Antarctica", "Asia", "Europe", "N. America", "Oceania", "S. America"))+
+  scale_y_discrete(name = "Total Views per Continent")
 
