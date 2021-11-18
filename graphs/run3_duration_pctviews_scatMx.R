738774891a08df7efab5cb96e4c6d59f83be8c49
a3032929 <91320050+a3032929@users.noreply.github.com>
2021-11-18 13:09:18 +0000

    doc: tided and updated rmd

--- graphs/run3_duration_pctviews_scatMx.R
+++
@@ -1,6 +0,0 @@
-
-# plot scatterplot matrix of video duration vs total views through video
-pairs(~video_duration + viewed_five_percent + viewed_ten_percent + viewed_twentyfive_percent + viewed_fifty_percent + 
-        viewed_seventyfive_percent+ viewed_ninetyfive_percent + viewed_onehundred_percent, 
-      data = run3unite,
-      main = "Figure XX: Total views throughout percentage phase of the Videos", pch = 19, upper.panel = NULL)
--- graphs/run3_duration_reduced_pctviews_scatMx.R
+++
@@ -1,4 +0,0 @@
-# plot scatterplot matrix of video duration vs total views through video
-pairs(~video_duration + viewed_twentyfive_percent + viewed_fifty_percent + viewed_seventyfive_percent, 
-      data = run3unite,
-      main = "Figure XX: Total views throughout percentage phase of the Videos", pch = 19, upper.panel = NULL)
