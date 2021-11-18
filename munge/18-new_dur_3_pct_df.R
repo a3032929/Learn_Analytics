5f5f8016eb3a5ad523e98b646ab0ec4058e95c2d
a3032929 <91320050+a3032929@users.noreply.github.com>
2021-11-18 15:19:37 +0000

    feat: created new df

---
+++ munge/18-new_dur_3_pct_df.R
@@ -0,0 +1,6 @@
+# transform 25/75/100% columns to enable graph plot
+new_dur_3_df_pct <- run3unite %>% 
+  select(video_duration, viewed_twentyfive_percent, viewed_seventyfive_percent, viewed_onehundred_percent) %>% 
+  gather(key = Phase, value = Value, viewed_twentyfive_percent:viewed_onehundred_percent)
+
+cache('new_dur_3_df_pct')
