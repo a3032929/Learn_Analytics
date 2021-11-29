cb82db268fb57b83b888e38ec7877e972a3117ab
a3032929 <91320050+a3032929@users.noreply.github.com>
2021-11-29 10:05:06 +0000

    graphs: updated graphs

--- graphs/line_cont3_pct_topic.R
+++ graphs/line_cont3_pct_topic.R
@@ -1,5 +1,14 @@
 # line graph of each continent throughout course
 line_cont3_pct_topic_V <- new_cont_3_df %>% 
+  mutate(step_title = fct_relevel(step_title, "1.1_Welcome to the course", "1.5_Privacy online and offline",
+                                  "1.14_Why would anyone want your data?", "1.17_Preserving privacy in cloud storage: privacy by design",
+                                  "1.19_Staying safe online: personal perspectives",
+                                  "2.1_Welcome to Week 2: payment security", "2.4_The evolving arms race of payment security",
+                                  "2.11_Exploring vulnerabilities in online payments",
+                                  "2.17_The million dollar contactless payment", "3.1_Welcome to Week 3: security in the future home",
+                                  "3.2_Devices in the future home",
+                                  "3.14_Exploring security: biometric authentication",
+                                  "3.15_Exploring security: the Access Control Live Lab")) %>% 
   ggplot( aes(x=step_title, y=Value, group=Continent, color=Continent))+
   geom_line(size=1)+
   geom_point(size=2)+
--- graphs/run3_totalview_topic_bar.R
+++ graphs/run3_totalview_topic_bar.R
@@ -1,5 +1,14 @@
 # plot horizontal bar chart of video topic vs total number of viewers
 run3_totalview_topic_bar_V <- run3unite %>% 
+  mutate(step_title = fct_relevel(step_title, "1.1_Welcome to the course", "1.5_Privacy online and offline",
+                                  "1.14_Why would anyone want your data?", "1.17_Preserving privacy in cloud storage: privacy by design",
+                                  "1.19_Staying safe online: personal perspectives",
+                                  "2.1_Welcome to Week 2: payment security", "2.4_The evolving arms race of payment security",
+                                  "2.11_Exploring vulnerabilities in online payments",
+                                  "2.17_The million dollar contactless payment", "3.1_Welcome to Week 3: security in the future home",
+                                  "3.2_Devices in the future home",
+                                  "3.14_Exploring security: biometric authentication",
+                                  "3.15_Exploring security: the Access Control Live Lab")) %>% 
   ggplot( aes(x=step_title, y=total_views))+
   geom_bar(stat = 'identity', fill="#0099f9")+
   geom_text(aes(label=total_views), hjust=-0.2, size=3.5)+
