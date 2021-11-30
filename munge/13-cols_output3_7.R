3ec9347deb2f58adf0e173031e74fa50e038e8f5
a3032929 <91320050+a3032929@users.noreply.github.com>
2021-11-30 13:20:00 +0000

    munge: updated munge folder

--- munge/13-cols_output3_7.R
+++ munge/13-cols_output3_7.R
@@ -1,19 +1,23 @@
 # add step_title and video_duration columns to tables
 output_3$step_title <- run3unite$step_title
 output_3$video_duration <- run3unite$video_duration
+cache('output_3')
 
 output_4$step_title <- run4unite$step_title
 output_4$video_duration <- run4unite$video_duration
+cache('output_4')
 
 output_5$step_title <- run5unite$step_title
 output_5$video_duration <- run5unite$video_duration
+cache('output_5')
 
 output_6$step_title <- run6unite$step_title
 output_6$video_duration <- run6unite$video_duration
+cache('output_6')
 
 output_7$step_title <- run7unite$step_title
 output_7$video_duration <- run7unite$video_duration
-
+cache('output_7')
 
 # reorganise columns
 output_3_ro <- output_3[,c(8,9,1,2,3,4,5,6,7)]
@@ -21,3 +25,8 @@ output_4_ro <- output_4[,c(8,9,1,2,3,4,5,6,7)]
 output_5_ro <- output_5[,c(8,9,1,2,3,4,5,6,7)]
 output_6_ro <- output_6[,c(8,9,1,2,3,4,5,6,7)]
 output_7_ro <- output_7[,c(8,9,1,2,3,4,5,6,7)]
+cache('output_3_ro')
+cache('output_4_ro')
+cache('output_5_ro')
+cache('output_6_ro')
+cache('output_7_ro')
--- munge/21-cols_output_views3_7.R
+++ munge/21-cols_output_views3_7.R
@@ -2,23 +2,27 @@
 output_3_view$step_title <- run3unite$step_title
 output_3_view$video_duration <- run3unite$video_duration
 output_3_view$total_views <- run3unite$total_views
+cache('output_3_view')
 
 output_4_view$step_title <- run4unite$step_title
 output_4_view$video_duration <- run4unite$video_duration
 output_4_view$total_views <- run4unite$total_views
+cache('output_4_view')
 
 output_5_view$step_title <- run5unite$step_title
 output_5_view$video_duration <- run5unite$video_duration
 output_5_view$total_views <- run5unite$total_views
+cache('output_5_view')
 
 output_6_view$step_title <- run6unite$step_title
 output_6_view$video_duration <- run6unite$video_duration
 output_6_view$total_views <- run6unite$total_views
+cache('output_6_view')
 
 output_7_view$step_title <- run7unite$step_title
 output_7_view$video_duration <- run7unite$video_duration
 output_7_view$total_views <- run7unite$total_views
-
+cache('output_7_view')
 
 # reorganise columns
 output_3_view_ro <- output_3_view[,c(5,6,7,1,2,3,4)]
@@ -26,3 +30,8 @@ output_4_view_ro <- output_4_view[,c(5,6,7,1,2,3,4)]
 output_5_view_ro <- output_5_view[,c(5,6,7,1,2,3,4)]
 output_6_view_ro <- output_6_view[,c(5,6,7,1,2,3,4)]
 output_7_view_ro <- output_7_view[,c(5,6,7,1,2,3,4)]
+cache('output_3_view_ro')
+cache('output_4_view_ro')
+cache('output_5_view_ro')
+cache('output_6_view_ro')
+cache('output_7_view_ro')
